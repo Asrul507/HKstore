b06d62a85ed7b4cc7ddabbf67526c74d100571f9
asrul <asrulmuh073@gmail.com>
2026-05-05 23:05:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -668,11 +668,4 @@ function loadDashboard() {
 
     document.getElementById("dashboardData").innerHTML = html;
   });
-  api({
-  action: "getDashboard",
-  bulan: bulan
-}).then(data => {
-
-  console.log("DASHBOARD DATA:", data); // 🔥 WAJIB
-
 }
